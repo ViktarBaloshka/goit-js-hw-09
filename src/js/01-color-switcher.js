2ea9c24aa0f4ed6002393166e7294ae43f15129e
Viktar Baloshka <vboloshko@gmail.com>
2022-05-09 22:13:47 +0200

    JS-HW-8-TASK-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+let timeoutId = null;
+const DELAY = 1000;
 
+let isActive = true;
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+btnStart.addEventListener('click', onStartChangeColor);
+btnStop.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+  btnStart.disabled = isActive;
+  btnStop.disabled = !isActive;
+  timeoutId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+  console.log(isActive);
+}
+
+function onStopChangeColor() {
+  clearInterval(timeoutId);
+  btnStart.disabled = !isActive;
+  btnStop.disabled = isActive;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

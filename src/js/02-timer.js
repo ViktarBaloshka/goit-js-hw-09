f0af1030ded5c385c48b186be5f4a9306e7ff5e7
Viktar Baloshka <vboloshko@gmail.com>
2022-05-09 22:15:10 +0200

    JS-HW-8-TASK-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,8 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const options = {
   enableTime: true,
